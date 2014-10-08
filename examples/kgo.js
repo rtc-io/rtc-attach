44c817e033bc6d424488ba79e3d1a5073a1e10a1
Damon Oehlman <damon.oehlman@gmail.com>
2014-10-08 17:50:49 +1100

    Refactor to use updated kgo api

--- examples/kgo.js
+++ examples/kgo.js
@@ -1,16 +1,15 @@
 var capture = require('rtc-capture');
-var attach = require('..');
+var attach = require('rtc-attach');
 var kgo = require('kgo');
 
-kgo
-('constraints', function(done) {
-  done(null, { video: true, audio: true })
-})
-('opts', function(done) {
-  done(null, { plugins: [
-    require('rtc-plugin-nicta-ios'),
-    require('rtc-plugin-temasys')
-  ]})
+kgo({
+  constraints: { video: true, audio: true },
+  opts: {
+    plugins: [
+      require('rtc-plugin-nicta-ios'),
+      require('rtc-plugin-temasys')
+    ]
+  }
 })
 ('capture', ['constraints', 'opts'], capture)
 ('attach', ['capture', 'opts'], attach)
